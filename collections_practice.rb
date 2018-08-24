f8077d883f8ee6ee9563346010ff5c787427d62b
Tristan Brown <tgbrown@stanford.edu>
2018-08-24 20:53:17 +0000

    automatically backed up by learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -55,3 +55,12 @@ end
 def sum_array(integers)
   integers.inject(0) { |result, element| result + element }
 end
+
+def add_s(array)
+  new_array = []
+  array.each_with_index.collect do |element, index|
+    array[1] = new_array[1]
+    new_array << element.unshift("s")
+  end
+  new_array 
+end
